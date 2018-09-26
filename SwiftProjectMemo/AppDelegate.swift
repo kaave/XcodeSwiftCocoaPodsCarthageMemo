11dec5beb210b13ec901af62806def613f0917e4
kaave <abe@framelunch.jp>
2018-09-26 16:12:35 +0900

    common: run autocorrect

--- SwiftProjectMemo/AppDelegate.swift
+++ SwiftProjectMemo/AppDelegate.swift
@@ -13,10 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 
-
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
-		
+
 		let window = UIWindow(frame: UIScreen.main.bounds)
 		window.rootViewController = UIViewController()
 		window.rootViewController?.view.backgroundColor = UIColor.blue
@@ -48,6 +47,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 	}
 
-
 }
-
