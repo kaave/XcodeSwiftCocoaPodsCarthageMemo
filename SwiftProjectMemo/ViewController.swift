29951222e0441cc53d8130f1dc728956e8b7760a
kaave <abe@framelunch.jp>
2018-09-26 14:58:52 +0900

    Initial commit

---
+++ SwiftProjectMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftProjectMemo
+//
+//  Created by 安部亨佑 on 2018/09/26.
+//  Copyright © 2018 kaave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
